507113b842b7f24535a03b5dee491ac5458a3b1d
arily <arily@arily.moe>
2019-06-20 00:45:34 +0900

    Add files via upload

--- wstunnel.js
+++ wstunnel.js
@@ -127,10 +127,11 @@ var branch_reverse_tcp = (src,port,addr,req) =>{
 
         var address = socket.address();
 
-        socket.on('data',function(data){
-            console.log('sent to ws',data.toString());
-            src.send(data);
-        });
+        //socket.on('data',function(data){
+        //    console.log('sent to ws',data.toString());
+        //    src.send(data);
+        //});
+        socket.pipe(src);
         src.on('message',(data) =>{
             console.log('sent to tcp',data.toString());
             socket.write(data);
