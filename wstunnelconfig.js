11c06674cb41d70d06d8bac139dc011ccb25ca5d
arily <arily@arily.moe>
2019-06-27 11:53:43 +0900

    Update wstunnelconfig.js

--- wstunnelconfig.js
+++ wstunnelconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
     wsServer : {
-        port        : process.env.PORT          || 5001,
-        output      : process.env.OUTPUT        || true,
-        outputLevel : process.env.OUTPUTLEVEL   || 3
+        port        : parseInt(process.env.PORT)        || 5001,
+        output      : process.env.OUTPUT == 'TRUE'      || true,
+        outputLevel : parseInt(process.env.OUTPUTLEVEL) || 3
     },
     Proxifier : {
-        reconnectWindow :process.env.RECONNECT_WINDOW || 5000,
-        reconnectEnabled:process.env.RECONNECT_ENABLE || true
+        reconnectWindow :parseInt(process.env.RECONNECT_WINDOW) || 5000,
+        reconnectEnabled:process.env.RECONNECT_ENABLE == 'TRUE' || true
     }
 };
