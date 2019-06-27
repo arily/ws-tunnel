756d42c0dc14c85da735edbd804952b769fa5ccc
arily <arily@arily.moe>
2019-06-28 02:48:42 +0900

    Add files via upload

---
+++ SocksServer.js
@@ -0,0 +1,11 @@
+//https://github.com/mscdex/socksv5
+var socks = require('socksv5');
+
+var srv = socks.createServer(function(info, accept, deny) {
+  accept();
+});
+srv.listen(10800, 'localhost', function() {
+  console.log('SOCKS server listening on port 10800');
+});
+
+srv.useAuth(socks.auth.None());
