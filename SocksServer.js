9add433f8c0b3260583883b976ee70f421060cf1
arily <lbsaaaaa@gmail.com>
2020-01-26 16:20:20 +0900

    formatting code

--- SocksServer.js
+++ SocksServer.js
@@ -1,13 +1,13 @@
 //https://github.com/mscdex/socksv5
-var socks = require('socksv5');
+const socks = require('socksv5');
 
-var srv = socks.createServer(function(info, accept, deny) {
-  accept();
+const srv = socks.createServer(function(info, accept, deny) {
+    accept();
 });
 srv.listen(10800, 'localhost', function() {
-  console.log('SOCKS server listening on port 10800');
+    console.log('SOCKS server listening on port 10800');
 });
 
 srv.useAuth(socks.auth.UserPassword(function(user, password, cb) {
-  cb(user === 'nodejs' && password === 'rules!');
+    cb(user === 'nodejs' && password === 'rules!');
 }));
